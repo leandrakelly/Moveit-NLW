7951ad1348b10f74e4a663689a4a8527b6456ab5
leandrakelly <leandrakell@gmail.com>
2021-02-22 16:39:04 -0300

    deleted buttons and put some css defined fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import {Button} from './Components/Button'
 import './Styles/Global.css'
 
 function App() {
   return (
     <>
-    <h1>Alo</h1>
-    <Button color = "blue">
-      Botão 01
-    </Button>
-    <Button color = "green">
-      Botão 02
-    </Button>
+    
     </>
   );
 }
