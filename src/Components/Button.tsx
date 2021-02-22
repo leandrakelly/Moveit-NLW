db9552711561ef703ac52ea031a0937cdd220a80
leandrakelly <leandrakell@gmail.com>
2021-02-22 15:38:56 -0300

    set increment in button with states

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,12 +1,24 @@
+import {useState} from 'react';
+
 interface Buttonprops{
     color: string;
     children: string;
 }
 
 export function Button(props: Buttonprops){
+    const [counter, setCounter] = useState(1);
+
+    function increment(){
+        setCounter(counter+1);
+    }
+
     return(
-        <button type="button" style={{ backgroundColor: props.color, }}>
-           {props.children} <strong>1</strong>
+        <button 
+        type="button" 
+        style={{ backgroundColor: props.color, }}
+        onClick = {increment}
+        >
+           {props.children} <strong>{counter}</strong>
         </button>
     );
 }
